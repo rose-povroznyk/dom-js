b615c3d08fbed86358b40855736cd8b2a2b1ca66
Rose <pas.une.mowgli@gmail.com>
2024-02-25 18:31:25 +0200

    Add changes

---
+++ bubbling.js
@@ -0,0 +1,17 @@
+// document.querySelector('article').addEventListener('click', function (e) {
+//   console.log('article hello');
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+// });
+
+// document.querySelector('section').addEventListener('click', function (e) {
+//   console.log('section hello');
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+// });
+
+// document.body.addEventListener('click', function (e) {
+//   console.log('Hello');
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+// });
